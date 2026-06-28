9f908fab151560e1a45b3fa01193f761dc637312
MasihTak <masihtak@outlook.com>
2026-06-28 03:40:11 +0200

    feat(notifications): register notification plugin in backend

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -7,6 +7,7 @@ pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_shell::init())
+        .plugin(tauri_plugin_notification::init())
         .plugin(
             tauri_plugin_sql::Builder::default()
                 .add_migrations(db::DB_URL, db::migrations())
